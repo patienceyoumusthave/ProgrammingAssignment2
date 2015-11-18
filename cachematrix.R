4be67e9bed5434c12bb6aebc6b8985a80d0680ff
Jeff Stewart <patienceyoumusthave@icloud.com>
2015-11-17 19:25:48 -0600

    Finished modifications to complete Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## R Programming  - Assignment 2
+## Jeff Stewart
+## Demonstration of caching a calculation using the "<<-" operator
+## 
+## makeCacheMatrix - function to calculate the inverse of a matrix
+##                 - requires an invertible matrix as input
+##                 - returns a list of functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinvert <- function(solve) m <<- solve
+    getinvert <- function() m
+    list(set = set, get = get,
+         setinvert = setinvert,
+         getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve - function to check for cached result, and calculate if not
+##            - takes result of "makeCacheMatrix" as input
+##            - checks to see if data is cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinvert()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinvert(m)
+    m
 }
